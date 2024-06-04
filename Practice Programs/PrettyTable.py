449595de2ebea40779ae32c70feb09e84d43154d
MYethishwar <yathishwerchintu@gmail.com>
2024-06-04 21:03:12 +0530

    Added PrettyTable Package

---
+++ Practice Programs/PrettyTable.py
@@ -0,0 +1,7 @@
+import prettytable as pt
+#Creating an prettytable object.
+table = pt.PrettyTable()
+table.add_column("Name",['Yethishwar','Chintu','Siddhartha','Sai kiran','Preethi'])
+table.add_column('Marks',[98,89,100,99,70])
+table.align = 'l' #Alligned data to the left
+print(table)
